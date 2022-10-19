0429087b51445fb4708856ff6b261159c6fd2960
Camxo <112751667+Camxo@users.noreply.github.com>
2022-10-20 10:23:30 +1300

    Add files via upload

---
+++ COUNTER APP/apps.js
@@ -0,0 +1,51 @@
+let h2 = document.getElementsByTagName('h2')[0];
+console.log(0)
+
+let btn1 = document.querySelector('.btn1')
+let btn2 = document.querySelector('.btn2')
+let btn3 = document.querySelector('.btn3')
+document.body.style.backgroundColor = 'yellow'  
+
+
+let score = 0
+
+btn1.addEventListener('click',()=>{
+    score--;
+    console.log(score);
+    h2.textContent = score;
+
+    if (score < 0) {
+        document.body.style.backgroundColor = 'red'
+    }else if (score == 0) {
+        document.body.style.backgroundColor = 'yellow'  
+    }else if (score > 0){
+        document.body.style.backgroundColor = 'green'
+    }
+})
+
+btn2.addEventListener('click', () => {
+    score = 0;
+    h2.textContent = score;
+
+    if (score < 0) {
+        document.body.style.backgroundColor = 'red'
+    }else if (score == 0) {
+        document.body.style.backgroundColor = 'yellow'  
+    }else if (score > 0){
+        document.body.style.backgroundColor = 'green'
+    }
+})
+
+btn3.addEventListener('click', () =>{
+    score++;
+    console.log(score);
+    h2.textContent = score;
+
+    if (score < 0) {
+        document.body.style.backgroundColor = 'red'
+    }else if (score == 0) {
+        document.body.style.backgroundColor = 'yellow'  
+    }else if (score > 0){
+        document.body.style.backgroundColor = 'green'
+    }
+})
